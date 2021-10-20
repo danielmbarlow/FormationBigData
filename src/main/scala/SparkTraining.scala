f11e593bbaa6748cc8bdf507a2a53b7ebfb82beb
danielmbarlow <danielmbarlow@gmail.com>
2021-10-20 13:50:42 +0200

    Spark select, joins, filters, aggregation

--- src/main/scala/SparkTraining.scala
+++ src/main/scala/SparkTraining.scala
@@ -1,9 +1,10 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{DateType, DoubleType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.{col, dayofmonth, lit, round, when}
 
 object SparkTraining {
   val nullable = false
-  val csvSchema: StructType = StructType {
+  val orderLineSchema: StructType = StructType {
     List(
       StructField("OrderLine", IntegerType, nullable),
       StructField("OrderId", IntegerType, nullable),
@@ -16,6 +17,22 @@ object SparkTraining {
     )
   }
 
+  var orderSchema: StructType = StructType {
+    List(
+      StructField("OrderId", IntegerType, nullable),
+      StructField("CustomerId", IntegerType, nullable),
+      StructField("CampaignId", IntegerType, nullable),
+      StructField("OrderDate", DateType, nullable),
+      StructField("City", StringType, nullable),
+      StructField("State", StringType, nullable),
+      StructField("ZipCode", StringType, nullable),
+      StructField("PaymentType", StringType, nullable),
+      StructField("TotalPrice", DoubleType, nullable),
+      StructField("NumOrderLines", IntegerType, nullable),
+      StructField("NumUnits", IntegerType, nullable)
+    )
+  }
+
   def main(args : Array[String]) : Unit = {
     val ss = SparkSession.builder()
       .appName("My Spark applications")
@@ -25,6 +42,7 @@ object SparkTraining {
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .config("spark.shuffle.compress", "true")
       .getOrCreate()
+    import ss.implicits._
 
     println("== Read from sequence")
     val sequence = Seq("Daniel", "Amelia", "Josh", "Saffron", "Amber")
@@ -39,7 +57,7 @@ object SparkTraining {
     println("== Read from small file")
     val textFileRdd = ss.sparkContext.textFile("C:/tmp/spark.txt")
       .flatMap(_.split(" "))
-//      .groupBy(w => w.matches(".*i.*")) <--- Celui ci m'a fait des problemes
+//      .groupBy(w => w.matches(".*i.*"))
 
     println("== Show RDD contents")
     textFileRdd.foreach(println(_))
@@ -47,21 +65,75 @@ object SparkTraining {
     println(textFileRdd.count())
 
     // This is used to transform RDDs to dataframes
-    import ss.implicits._
     println("== Show data frame")
     val dataFrame = textFileRdd.toDF()
 
     dataFrame.show()
 
     println("== Read from big CSV file")
-    val df_orders = ss.read
+    val orderLines = ss.read
       .format("com.databricks.spark.csv")
       .option("delimiter", "\t")
       .option("header", "true")
-      .schema(csvSchema)
+      .schema(orderLineSchema)
       .load("C:/tmp/orderline.txt")
 
-    df_orders.show(15)
+    val orders = ss.read
+      .format("com.databricks.spark.csv")
+      .option("delimiter", "\t")
+      .option("header", "true")
+      .schema(orderSchema)
+      .load("C:/tmp/orders.txt")
+      .select(
+        $"OrderId",
+        $"City"
+      )
+
+//    orders.show(15)
+//    orderLines.show(15)
+//    orderLines.printSchema()
+
+//    println(orderLines.columns)
+
+    println("== Show transformed data")
+
+    val taxRate = 0.175
+    val totalPrice = col("TotalPrice")
+    val taxes = totalPrice * taxRate
+    val promo = when(totalPrice < 20, 0).otherwise(
+      when((lit(200) < totalPrice) && (totalPrice < lit(600)), 0.05)
+        .otherwise(0.07)
+    )
+    val promoAmount = promo * totalPrice
+    val totalBill = round(totalPrice - promoAmount + taxes)
+
+    val priceDataFrame = orderLines.select(
+      $"OrderId",
+      $"UnitPrice".as("Unit price"),
+      $"NumUnits",
+      round(totalPrice, 2).as("TotalPrice"),
+      $"OrderLine".cast(StringType),
+      dayofmonth(col("ShipDate")).as("ShipDayOfMonth"),
+      promo.as("Promo"),
+      promoAmount.as("PromoAmount"),
+      totalBill.as("TotalBill")
+    )
+    priceDataFrame.show(10)
+
+    println("== Show filtered data")
+    val filtered = priceDataFrame
+      .filter(col("NumUnits") > lit(2))
+      .filter(col("ShipDayOfMonth").isin(1, 15, 30))
+
+    println("== Show joined data")
+    val joined = filtered.join(orders, orders("OrderId") === filtered("OrderId"), "inner")
+
+    joined.show(15)
+
+    println("== Show aggregated data")
+    joined.groupBy("City")
+      .sum("TotalPrice").as("TotalPrice")
+      .show(15)
 
     println("== Read from orc file")
     val df_orc = ss.read.orc("C:/tmp/orc/*.orc")
